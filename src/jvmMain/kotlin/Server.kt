413a179c754e32ef25fdd69a98fad37e86182b8a
limseongkyu <96x60812@gmail.com>
2023-05-06 22:53:36 +0900

    Feat: ktor plugins

--- src/jvmMain/kotlin/Server.kt
+++ src/jvmMain/kotlin/Server.kt
@@ -1,14 +1,28 @@
-import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import io.ktor.server.application.*
+import io.ktor.server.plugins.compression.*
+import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.plugins.cors.routing.*
 
 fun main() {
     embeddedServer(Netty, 9090, module = Application::myApplicationModule).start(wait = true)
 }
 
 fun Application.myApplicationModule() {
+    install(ContentNegotiation) { json() }
+    install(CORS) {
+        allowMethod(io.ktor.http.HttpMethod.Get)
+        allowMethod(io.ktor.http.HttpMethod.Post)
+        allowMethod(io.ktor.http.HttpMethod.Delete)
+        anyHost()
+    }
+    install(Compression) {
+        gzip()
+    }
     routing {
         get("/hello") {
             call.respondText("Hello, API !")
