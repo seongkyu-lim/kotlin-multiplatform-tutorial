c4368daed757b8ea0d1de416091f9978e90be452
limseongkyu <96x60812@gmail.com>
2023-05-06 22:45:34 +0900

    Feat: hello API

--- src/jvmMain/kotlin/Server.kt
+++ src/jvmMain/kotlin/Server.kt
@@ -1,3 +1,17 @@
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
 fun main() {
-    println("Hello, JVM!")
+    embeddedServer(Netty, 9090, module = Application::myApplicationModule).start(wait = true)
+}
+
+fun Application.myApplicationModule() {
+    routing {
+        get("/hello") {
+            call.respondText("Hello, API !")
+        }
+    }
 }
